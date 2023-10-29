b67cc8ad5464039eb8660ddd3052d07ff5f204a9
kiriti07 <ch.kiriti@gmail.com>
2023-10-29 06:09:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-# app.py
-from flask import Flask, render_template, request
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -8,8 +7,14 @@ def index():
     if request.method == 'POST':
         firstname = request.form.get('firstname')
         lastname = request.form.get('lastname')
-        return f"Hello, {firstname} {lastname}!"
-    return render_template('index.html')
+        return f'Hello {firstname} {lastname}!'
+    return '''
+        <form method="post">
+            First Name: <input type="text" name="firstname">
+            Last Name: <input type="text" name="lastname">
+            <input type="submit" value="Submit">
+        </form>
+    '''
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
